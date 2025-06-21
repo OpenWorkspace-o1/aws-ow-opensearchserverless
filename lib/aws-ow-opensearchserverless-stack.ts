425f781008fe4dcd70c926347139881ae608ccce
Trung Dinh <xamarindevelopervietnam@outlook.com>
2025-06-21 21:49:04 +0700

    Add interface AwsOwOpensearchserverlessStackProps

--- lib/aws-ow-opensearchserverless-stack.ts
+++ lib/aws-ow-opensearchserverless-stack.ts
@@ -1,9 +1,33 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export interface AwsOwOpensearchserverlessStackProps extends cdk.StackProps {
+  /** Resource prefix for all AWS resources */
+  readonly resourcePrefix: string;
+  /** AWS region where resources will be deployed */
+  readonly deployRegion: string | undefined;
+  /** Deployment environment (e.g., development, staging, production) */
+  readonly deployEnvironment: string;
+  /** Name of the application */
+  readonly appName: string;
+  /** Type of VPC subnet (e.g., public, private) */
+  readonly vpcSubnetType: string;
+  /** Owner or team responsible for the resources */
+  readonly owner: string;
+  /** ID of the VPC where resources will be deployed */
+  readonly vpcId: string;
+  /** List of private subnet IDs in the VPC */
+  readonly vpcPrivateSubnetIds: string[];
+  /** List of Availability Zones for private subnets */
+  readonly vpcPrivateSubnetAzs: string[];
+  /** List of route table IDs for private subnets */
+  readonly vpcPrivateSubnetRouteTableIds: string[];
+  /** List of collection names */
+  readonly collectionNames: string[];
+}
 
 export class AwsOwOpensearchserverlessStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: AwsOwOpensearchserverlessStackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
