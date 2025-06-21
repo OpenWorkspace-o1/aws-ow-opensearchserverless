8d7792c27497bef5eb1e55ff7fb40e9f2656370a
Trung Dinh <xamarindevelopervietnam@outlook.com>
2025-06-21 21:12:52 +0700

    Init commit

---
+++ test/aws-ow-opensearchserverless.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsOwOpensearchserverless from '../lib/aws-ow-opensearchserverless-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-ow-opensearchserverless-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsOwOpensearchserverless.AwsOwOpensearchserverlessStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
